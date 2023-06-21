2a475cba7b7f8c831d588ee834efb6d17a678375
sphetnin <sphetnin@gmail.com>
2023-06-21 20:20:39 +0700

    dddd

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,42 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import SignIn from './route/SignIn.jsx'
 import SignUp from './route/Register.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/signin",
-    element: <SignIn />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
+// import { createBrowserRouter, RouterProvider, BrowserRouter } from "react-router-dom";
 
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "/signin",
+//     element: <SignIn />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <SignUp />,
+//   },
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+// ]);
 
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// )
+export default function App1() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App1 />);
